909536df5e8ea2ceb2f2d09738b35c81031beb0a
NakoNachev <nako.n.nachev@gmail.com>
2024-01-13 17:18:00 +0100

    add indexOutOfRange check for soliton, create a small input for testing purposes

--- soliton.py
+++ soliton.py
@@ -3,13 +3,13 @@ import math
 def robust_soliton_distribution(K: int, c: float, delta: float):
     # Calculate parameter 's'
     s = c * math.sqrt(K * math.log(K / delta) ** 2)
-
     # Define tau(d)
     tau = [1 / K if d == 1 else 1 / (d * (d - 1)) for d in range(1, K + 1)]
-
     # Define rho(d)
     rho = [s / (K * d) if d <= math.floor(K / s) - 1 else 0 for d in range(1, K + 1)]
-    rho[math.floor(K / s) - 1] = s * math.log(s / delta) / K
+    index = math.floor(K / s) - 1
+    if index <= K - 1:   # otherwise fails with index out of range
+        rho[math.floor(K / s) - 1] = s * math.log(s / delta) / K
 
     # Combine to form mu(d)
     mu = [tau_d + rho_d for tau_d, rho_d in zip(tau, rho)]
